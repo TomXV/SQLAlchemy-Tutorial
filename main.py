bb563789038293b7d4ea3922f66c4b27e7f61f10
Tom_XV <55909638+TomXV@users.noreply.github.com>
2022-02-10 22:41:46 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+import database
+
+
+def method_continue():
+    print(
+"""
+----------------------------------------------------------------
+Continue?
+----------------------------------------------------------------
+Yes: 1
+No: 0
+----------------------------------------------------------------
+"""
+)
+    sel_num = input("Number?: ")
+    if sel_num == "1":
+        main()
+    else:
+        exit()
+
+def main():
+    print(
+"""
+----------------------------------------------------------------
+CRUD MENU
+----------------------------------------------------------------
+Create: 1
+Read: 2
+Update: 3
+Delete: 4
+Exit: 5
+----------------------------------------------------------------
+"""
+)
+
+
+    sel_menu_num = input("Number?: ")
+
+    if sel_menu_num == "1":
+        database.create()
+        method_continue()
+    elif sel_menu_num == "2":
+        database.read()
+        method_continue()
+    elif sel_menu_num == "3":
+        database.update()
+        method_continue()
+    elif sel_menu_num == "4":
+        database.delete()
+        method_continue()
+    elif sel_menu_num == "5":
+        exit()
+    else:
+        main()
+
+
+if __name__ == '__main__':
+    main()
